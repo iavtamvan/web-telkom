8e4b5659ef81484358d37f7cc1b50bf819bee9f6
iavtamvan <ade.fajr.ariav@gmail.com>
2018-11-06 01:39:08 -0800

    commit point #19

--- api/telkom_apps/controller/register_api.php
+++ api/telkom_apps/controller/register_api.php
@@ -24,7 +24,7 @@ VALUE ('$nama_user', '$wilayah_user', '$datel_user','$no_hp_user', '$username', 
         $pisahkanID = mysqli_fetch_assoc($querryIdUser);
         $ambilIDUser = "".$pisahkanID['is_user'];
         $responses["error_msg"] = "Register berhasil! Silahkan login";
-        $responses["id_user"] = $pisahkanID;
+        $responses["id_user"] = $ambilIDUser;
         echo json_encode($responses);
 
     } else{
